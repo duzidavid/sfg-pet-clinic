f65360f8256f52710a3b8c0050c4f4360a80cb06
duzidavid <david.duzi@teliacompany.com>
2020-01-07 18:11:07 +0100

    Initial Project Add

---
+++ src/test/java/cz/duzi/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.duzi.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
