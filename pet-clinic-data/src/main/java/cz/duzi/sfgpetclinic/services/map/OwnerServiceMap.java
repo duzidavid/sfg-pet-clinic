08be0af60376e37c4aed922e0eb3e0c38a79427b
duzidavid <david.duzi@teliacompany.com>
2020-01-12 19:46:34 +0100

    Impl Map based Services. Closes #6.

---
+++ pet-clinic-data/src/main/java/cz/duzi/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,35 @@
+package cz.duzi.sfgpetclinic.services.map;
+
+import cz.duzi.sfgpetclinic.model.Owner;
+import cz.duzi.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+
+}
