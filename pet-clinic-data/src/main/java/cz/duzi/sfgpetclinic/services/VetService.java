c443e49ba2de2e06e56a1c550ce79ed699be019d
duzidavid <david.duzi@teliacompany.com>
2020-01-12 15:54:09 +0100

    Implement Interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/cz/duzi/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package cz.duzi.sfgpetclinic.services;
+
+import cz.duzi.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
