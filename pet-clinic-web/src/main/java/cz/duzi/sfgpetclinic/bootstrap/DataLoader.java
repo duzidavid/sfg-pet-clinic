27932b6a30f2226ce9a9f6d3621f80e91eea3794
duzidavid <david.duzi@teliacompany.com>
2020-01-13 18:38:42 +0100

    Add bootstrap loader for data. Closes #14.

---
+++ pet-clinic-web/src/main/java/cz/duzi/sfgpetclinic/bootstrap/DataLoader.java
@@ -0,0 +1,60 @@
+package cz.duzi.sfgpetclinic.bootstrap;
+
+import cz.duzi.sfgpetclinic.model.Owner;
+import cz.duzi.sfgpetclinic.model.Vet;
+import cz.duzi.sfgpetclinic.services.OwnerService;
+import cz.duzi.sfgpetclinic.services.VetService;
+import cz.duzi.sfgpetclinic.services.map.OwnerServiceMap;
+import cz.duzi.sfgpetclinic.services.map.VetServiceMap;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final OwnerService ownerService;
+    private final VetService vetService;
+
+    public DataLoader() {
+        ownerService = new OwnerServiceMap();
+        vetService = new VetServiceMap();
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        owner1.setFirstName("Michael");
+        owner1.setLastName("Weston");
+
+        ownerService.save(owner1);
+
+        Owner owner2 = new Owner();
+        owner2.setId(2L);
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Glennane");
+
+        ownerService.save(owner2);
+
+        System.out.println("Loaded owners.....");
+
+        Vet vet1 = new Vet();
+        vet1.setId(1L);
+        vet1.setFirstName("Sam");
+        vet1.setLastName("Axe");
+
+        vetService.save(vet1);
+
+        Vet vet2 = new Vet();
+        vet2.setId(2L);
+        vet2.setFirstName("Dawid");
+        vet2.setLastName("Duzi");
+
+        vetService.save(vet2);
+
+        System.out.println("Loaded Vets......");
+
+
+    }
+}
