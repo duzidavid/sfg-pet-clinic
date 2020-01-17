2c12cdecb06585653d4305f002a1ef9d2a1cb59f
duzidavid <david.duzi@teliacompany.com>
2020-01-17 12:40:24 +0100

    Added Specialties Map impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/cz/duzi/sfgpetclinic/services/SpecialitesService.java
@@ -0,0 +1,6 @@
+package cz.duzi.sfgpetclinic.services;
+
+import cz.duzi.sfgpetclinic.model.Specialty;
+
+public interface SpecialitesService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/cz/duzi/sfgpetclinic/services/map/SpecialtiesServiceMap.java
@@ -0,0 +1,35 @@
+package cz.duzi.sfgpetclinic.services.map;
+
+import cz.duzi.sfgpetclinic.model.Specialty;
+import cz.duzi.sfgpetclinic.services.SpecialitesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtiesServiceMap extends AbstractMapService<Specialty, Long> implements SpecialitesService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
