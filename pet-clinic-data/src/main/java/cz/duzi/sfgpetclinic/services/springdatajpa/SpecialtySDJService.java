8aed1cbbbbb54483d80ceabbd8a310a0c57af319
duzidavid <david.duzi@teliacompany.com>
2020-01-21 18:53:54 +0100

    Added Spring Data JPA service for Specialty. Closes #44.

---
+++ pet-clinic-data/src/main/java/cz/duzi/sfgpetclinic/services/springdatajpa/SpecialtySDJService.java
@@ -0,0 +1,48 @@
+package cz.duzi.sfgpetclinic.services.springdatajpa;
+
+import cz.duzi.sfgpetclinic.model.Specialty;
+import cz.duzi.sfgpetclinic.repositories.SpecialtyRepository;
+import cz.duzi.sfgpetclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialtySDJService implements SpecialtyService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtySDJService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return specialtyRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty specialty) {
+        return specialtyRepository.save(specialty);
+    }
+
+    @Override
+    public void delete(Specialty specialty) {
+        specialtyRepository.delete(specialty);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialtyRepository.deleteById(id);
+    }
+}
