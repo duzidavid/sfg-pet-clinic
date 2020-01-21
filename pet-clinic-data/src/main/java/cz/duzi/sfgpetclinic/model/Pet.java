8317a27e79c92d6fa155c7ab9583e73b47b464f5
duzidavid <david.duzi@teliacompany.com>
2020-01-21 15:02:33 +0100

    Updated owner with JPA entity mapping. Closes #36.

--- pet-clinic-data/src/main/java/cz/duzi/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/cz/duzi/sfgpetclinic/model/Pet.java
@@ -1,13 +1,25 @@
 package cz.duzi.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity{
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
-    private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
 
     public String getName() {
         return name;
