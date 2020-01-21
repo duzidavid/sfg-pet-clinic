de01cd610e9a8a07c4c66c215541580f07a55215
duzidavid <david.duzi@teliacompany.com>
2020-01-21 18:26:23 +0100

    Added Spring Data JPA service for Owners. Closes #40.

---
+++ pet-clinic-data/src/main/java/cz/duzi/sfgpetclinic/services/springdatajpa/OwnerSDJService.java
@@ -0,0 +1,53 @@
+package cz.duzi.sfgpetclinic.services.springdatajpa;
+
+import cz.duzi.sfgpetclinic.model.Owner;
+import cz.duzi.sfgpetclinic.repositories.OwnerRepository;
+import cz.duzi.sfgpetclinic.services.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerSDJService implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+
+    public OwnerSDJService(OwnerRepository ownerRepository) {
+        this.ownerRepository = ownerRepository;
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().iterator().forEachRemaining(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return ownerRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return ownerRepository.save(owner);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        ownerRepository.delete(owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        ownerRepository.deleteById(id);
+    }
+}
